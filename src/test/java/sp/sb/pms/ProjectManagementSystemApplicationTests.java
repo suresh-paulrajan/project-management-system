19d8fa55d6beff3f149a4bb9055ff80aff8c273e
Suresh Paulrajan <suresh@lastdesk.net>
2022-03-13 22:59:53 +0530

    Base SpringBoot Project

---
+++ src/test/java/sp/sb/pms/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.sb.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
